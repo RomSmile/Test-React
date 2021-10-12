c78abae363ff8bed7b851d37fe91cf75ddd051ff
Roman Yacenko <ryacenko3@gmail.com>
2021-10-12 17:52:29 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getCountries } from './api/api';
 import './App.scss';
-
-interface Props {
-  onClick: () => void;
-}
-
-export const Provider: React.FC<Props> = React.memo(
-  ({ onClick, children }) => (
-    <button
-      type="button"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  ),
-);
+import { Header } from './components/Header/Header';
+import { ListOfCountries } from './components/ListOfCountries/ListOfCountries';
+import { ModalBlock } from './components/ModalBlock/ModalBlock';
 
 export const App: React.FC = () => {
+  const [countries, getListOfCountries] = useState<Country[]>([]);
+  const [name, setName] = useState<string>('');
+  const [id, setId] = useState<string>('');
+  const [show, setShow] = useState<boolean>(false);
+
+  useEffect(() => {
+    (async () => {
+      const response = await getCountries();
+
+      let countriesList = response.Countries;
+
+      if (name.length > 0) {
+        countriesList = countriesList.filter(
+          country => country.Country.toLowerCase().includes(name.toLocaleLowerCase()),
+        );
+      }
+
+      getListOfCountries(countriesList);
+    })();
+  }, [name]);
+
   return (
     <div className="starter">
-      <Provider onClick={() => ({})}>
-        <TodoList />
-      </Provider>
+      <Header setName={setName} />
+      <ListOfCountries countries={countries || []} setId={setId} setShow={setShow} />
+      {show
+        ? <ModalBlock id={id} setShow={setShow} />
+        : ''}
     </div>
   );
 };
